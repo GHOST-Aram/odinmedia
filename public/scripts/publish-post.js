b5d6b9676cb3064463bf819d1679cd59b4adcb57
GHOST-Aram <owinoojala@gmail.com>
2023-09-25 10:20:26 +0300

    refactor: Make button manipulation functions reusable in           sending creating new post
             -Inlcude button element as a parameter in
              changeTextContent, disableButton, enableButton
             functions
             -Supply event.target as button value in click
              event handler function

--- public/scripts/publish-post.js
+++ public/scripts/publish-post.js
@@ -1,12 +1,10 @@
 const publishBtn = document.querySelector('section#posts .new-post button')
 const textArea = document.querySelector('section#posts .new-post textarea')
 
-publishBtn.addEventListener('click', async() =>{
-    console.log('Elons')
-    console.log('Publising post', textArea.value)
+publishBtn.addEventListener('click', async(event) =>{
     if(textArea.value.trim() !== ''){
-        changeButtonText('Publishing ...')
-        disableButton(publishBtn)
+        changeButtonText(event.target, 'Publishing ...')
+        disableButton(event.target)
 
         const postData = {
             text: textArea.value,
@@ -16,39 +14,43 @@ publishBtn.addEventListener('click', async() =>{
         }
 
         try {
-            const response = await fetch('http://localhost:8000/content/post', {
-                method: 'POST',
-                body: JSON.stringify(postData),
-                mode: 'cors',
-                headers: {
-                    "Content-type": "application/json"
-                }
-            })
-            const data = await response.json()
-            console.log(data)
-            response.post ? window.location.reload() 
-                :data.error && console.error(data.error)
-
+            const response = await sendNewPOST(
+                'http://localhost:8000/content/post',
+                postData
+            )
+            console.log(await response.json())
         } catch (error) {
             console.error('Unexpected error occured: ', error)
         } finally{
-            changeButtonText('Publish Post')
-            enableButton()
+            changeButtonText(event.target, 'Publish Post')
+            enableButton(event.target)
         }
         
     }
 })
 
-const disableButton = () =>{
-    publishBtn.setAttribute('disabled', 'true')
-    publishBtn.classList.add('opacity-50')
+const disableButton = (btn) =>{
+    btn.setAttribute('disabled', 'true')
+    btn.classList.add('opacity-50')
 }
 
-const enableButton = () =>{
-    publishBtn.removeAttribute('disabled')
-    publishBtn.classList.remove('opacity-50')
+const enableButton = (btn) =>{
+    btn.removeAttribute('disabled')
+    btn.classList.remove('opacity-50')
 }
 
-const changeButtonText = (text) =>{
-    publishBtn.textContent = text
+const changeButtonText = (btn, text) =>{
+    btn.textContent = text
+}
+
+const sendNewPOST = async(url, data) =>{
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        mode: 'cors',
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    return response
 }
