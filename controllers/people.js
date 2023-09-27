c97bb5bc4beb2b48654374bd802ea520d6e87b03
GHOST-Aram <owinoojala@gmail.com>
2023-09-27 10:55:08 +0300

    feat: Create Sent requests display UI

---
+++ controllers/people.js
@@ -0,0 +1,51 @@
+export const get_all_people = (req, res) =>{
+    res.render('people', 
+    { 
+        title: 'People', 
+        user: {
+            name: 'Irvine Johnson',
+            pictureUrl: 'https://randomuser.me/api/portraits/men/83.jpg'
+        },
+        people
+    })
+}
+
+export const get_sent_requests = (req, res) =>{
+    res.render('requests-sent', 
+    { 
+        title: 'People', 
+        user: {
+            name: 'Irvine Johnson',
+            pictureUrl: 'https://randomuser.me/api/portraits/men/83.jpg'
+        },
+        people
+    })
+}
+
+const people = [
+    {
+        name: 'John Khan',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/23.jpg',
+        mutuaFriends: 20,
+    },
+    {
+        name: 'Erickson Denis',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/13.jpg',
+        mutuaFriends: 10,
+    },
+    {
+        name: 'Fransk Odoi',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/33.jpg',
+        mutuaFriends: 12,
+    },
+    {
+        name: 'Fred Kennedy',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/73.jpg',
+        mutuaFriends: 21,
+    },
+    {
+        name: 'Erick Bell',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/12.jpg',
+        mutuaFriends: 28,
+    },
+]
--- routes/people.js
+++ routes/people.js
@@ -1,45 +1,11 @@
+import {    
+    get_all_people, 
+    get_sent_requests 
+} from "../controllers/people.js";
 import { Router } from "../zghost/app/init.js";
 
 const peopleRouter = Router()
 
-peopleRouter.get('/', (req, res) =>{
-    res.render('people', 
-    { 
-        title: 'People', 
-        user: {
-            name: 'Irvine Johnson',
-            pictureUrl: 'https://randomuser.me/api/portraits/men/83.jpg'
-        },
-        people
-    })
-})
-
-const people = [
-    {
-        name: 'John Khan',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/23.jpg',
-        mutuaFriends: 20,
-    },
-    {
-        name: 'Erickson Denis',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/13.jpg',
-        mutuaFriends: 10,
-    },
-    {
-        name: 'Fransk Odoi',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/33.jpg',
-        mutuaFriends: 12,
-    },
-    {
-        name: 'Fred Kennedy',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/73.jpg',
-        mutuaFriends: 21,
-    },
-    {
-        name: 'Erick Bell',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/12.jpg',
-        mutuaFriends: 28,
-    },
-]
-
+peopleRouter.get('/', get_all_people)
+peopleRouter.get('/requests/sent', get_sent_requests)
 export {peopleRouter}
