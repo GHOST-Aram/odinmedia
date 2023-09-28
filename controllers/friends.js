46cfe7f7d3cab99efc9bafd363371c4d94b2f9de
GHOST-Aram <owinoojala@gmail.com>
2023-09-28 11:32:22 +0300

    chore: Link current user picture on small screens to current        user profile

--- controllers/friends.js
+++ controllers/friends.js
@@ -3,10 +3,6 @@ export const get_all_friends = (req, res) =>{
     res.render('friends', 
     { 
         title: 'Friends', 
-        user: {
-            name: 'Irvine Johnson',
-            pictureUrl: 'https://randomuser.me/api/portraits/men/83.jpg'
-        },
         people
     })
 }
