b83a2f1dd7450b2c1f8354adf6ca607149f4f5c1
GHOST-Aram <owinoojala@gmail.com>
2023-09-28 11:16:42 +0300

    chore: move people data to dummy data file

--- controllers/friends.js
+++ controllers/friends.js
@@ -1,3 +1,4 @@
+import { people } from "../data.js"
 export const get_all_friends = (req, res) =>{
     res.render('friends', 
     { 
@@ -10,30 +11,3 @@ export const get_all_friends = (req, res) =>{
     })
 }
 
-const people = [
-    {
-        name: 'John Khan',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/23.jpg',
-        mutuaFriends: 20,
-    },
-    {
-        name: 'Erickson Denis',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/13.jpg',
-        mutuaFriends: 10,
-    },
-    {
-        name: 'Fransk Odoi',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/33.jpg',
-        mutuaFriends: 12,
-    },
-    {
-        name: 'Fred Kennedy',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/73.jpg',
-        mutuaFriends: 21,
-    },
-    {
-        name: 'Erick Bell',
-        pictureUrl: 'https://randomuser.me/api/portraits/men/12.jpg',
-        mutuaFriends: 28,
-    },
-]
