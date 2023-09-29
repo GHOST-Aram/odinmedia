5030839806e6214cc2d70abe2c5d8b274e915f2e
GHOST-Aram <owinoojala@gmail.com>
2023-09-29 13:40:22 +0300

    improvement: Set background for currently active header navlinks              on small screens
                -Set blue background if current url includes
                 the textcontent of the span element within the buttons

--- public/scripts/navlinks.js
+++ public/scripts/navlinks.js
@@ -37,4 +37,24 @@ window.addEventListener('load', () => {
         }
         
     } catch (error) {}
+
+    //Small  Screens
+    try {
+        const smallScreenNavLinkSpans = document.querySelectorAll('aside.small-screen nav button span') 
+        const profilePicBtn = document.querySelector('aside.small-screen button.profile')
+
+        smallScreenNavLinkSpans.forEach(span =>{ 
+            if(new RegExp(span.textContent.toLocaleLowerCase())
+            .test(locationHref)){
+                span.parentElement.classList.remove('text-slate-600')
+                span.parentElement.classList.add('text-blue-500', 'bg-white')
+            }
+        })
+        if(locationHref.includes('/profiles/me')){
+            profilePicBtn.classList.add('bg-white')
+        }
+
+    } catch (error) {
+        
+    }
 })
