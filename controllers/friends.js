a1611e67865766a0747ed7e374fdb1a88e636581
GHOST-Aram <owinoojala@gmail.com>
2023-10-01 15:06:01 +0300

    chore: Adjust top padding of index page

--- controllers/friends.js
+++ controllers/friends.js
@@ -3,6 +3,7 @@ import { people } from "../data.js"
 export const unfriend = (req, res) => {
     res.redirect('/friends')
 }
+
 export const get_all_friends = (req, res) =>{
     res.render('friends', 
     { 
