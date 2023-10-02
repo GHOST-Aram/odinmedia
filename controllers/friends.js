f8313bf2b012e4ad9ed986bdbbc870013e2f89a2
GHOST-Aram <owinoojala@gmail.com>
2023-10-02 16:57:30 +0300

    chore: Link number of friends on user profile page to their        friends list
          -Change path for fetching friends of users

--- controllers/friends.js
+++ controllers/friends.js
@@ -5,10 +5,13 @@ export const unfriend = (req, res) => {
 }
 
 export const get_all_friends = (req, res) =>{
+    const id = req.params.id
+
+    const profile = people.find(person => person.id === id)
     res.render('friends', 
     { 
-        title: 'Friends', 
-        heading: 'Friends',
+        title: `Friends of ${profile.name}`, 
+        heading: `Friends of ${profile.name}`,
         people
     })
 }
--- routes/friends.js
+++ routes/friends.js
@@ -3,7 +3,7 @@ import { Router } from "../zghost/app/init.js";
 
 const friendsRouter = Router()
 
-friendsRouter.get('/', get_all_friends)
+friendsRouter.get('/:id/all', get_all_friends)
 friendsRouter.get('/:id/unfriend', unfriend)
 
 export {friendsRouter}
