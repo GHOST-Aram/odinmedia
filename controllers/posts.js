58b63ebd079a055ef42bc6fb09fd81cb2516f366
GHOST-Aram <owinoojala@gmail.com>
2023-10-06 09:17:17 +0300

    feat: Write controller function to get one post
         -Get one post from DB, populate authors and comments
         -Format post properties

--- controllers/posts.js
+++ controllers/posts.js
@@ -31,3 +31,51 @@ export const get_posts = async (req, res) => {
     }
 }
 
+export const get_one_post = async(req, res) =>{
+    const id = req.params.id
+
+    try {
+        const post = await Post.findById(id)
+        .populate({
+                path: 'author',
+                select: 'first_name last_name pictureUrl _id'
+        })
+        .populate(
+            {
+                path: 'comments',
+                populate: {
+                    path: 'author',
+                    select: 'first_name last_name pictureUrl _id'
+                }
+            }
+        )
+
+        const formattedPost = {
+            id: post._id.toString(),
+            post_content: post.post_content,
+            author: {
+                id: post.author._id.toString(),
+                name: `${post.author.first_name} ${post.author.last_name}`,
+                pictureUrl: post.author.pictureUrl
+            },
+            comments: post.comments.map(comment => ({
+                author: formatAuthor(comment.author),
+                text: comment.text
+            })),
+            likes: post.likes.length,
+            reposts: post.reposts.length,
+            createdAt: formatDate(post.createdAt)
+        }
+        
+        res.render('post-details', { 
+            title: `Post | ${id}`, 
+            heading: 'Post', 
+            post:formattedPost
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')   
+    }
+   
+}
+
