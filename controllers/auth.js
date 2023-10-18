cd41ae1f1aa263efa3ab600fccb379d9290d5220
GHOST-Aram <owinoojala@gmail.com>
2023-10-18 12:31:48 +0300

    chore: Validate user sign up credentials in the backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,59 @@
 import { Authenticator } from "../zghost/app/init.js"
 import { createUserWithHashedPassword } from "../utils/auth.js"
 import { validationResult, validator } from "../zghost/utils/validator.js"
+import { User } from "../zghost/db/User.js"
+import { matchedData } from "express-validator"
 
 
-export const creat_user = (req, res) => {
-   try {
-    createUserWithHashedPassword(req)
-    res.redirect('/auth/login')
+export const creat_user = [
+    validator.validatePlainText('first_name', { identifier: 'First Name' }),
+    validator.validatePlainText('last_name', { identifier: 'Last Name'}),
+    validator.validateEmail('email').custom(
+        async(email) =>{
+            const user = await User.findOne({ email })
+            
+            if(user){
+                throw new Error (`The email ${email} has been registered.`)
+            }
+        }
+    ),
+    validator.validatePassword('password'),
+    validator.validatePassword('confirm_password').custom(
+        async(confirm_password, { req }) =>{
+            const { password } = matchedData(req)
 
-   } catch (error) {
-       console.log(error)
-        res.status(500).send('Internal server error')
-   } 
-}
+            if(confirm_password !== password ){
+                throw new Error ('Password and confirm password must be identical')
+            }
+        }
+    ),
 
+    (req, res) => {
+        const errors = validationResult(req)
+        console.log(errors.array())
+        if(!errors.isEmpty()){
+            res.render('accounts/sign-up', 
+                {errors: errors.array(), title: 'Sign Up Error'}
+            )
+        } else {
+            try {
+                createUserWithHashedPassword(req)
+                res.redirect('/auth/login')
+    
+            } catch (error) {
+                console.log(error)
+                    res.status(500).send('Internal server error')
+            } 
+        }
+
+    }
+]
 export const get_login_form = (req, res) =>{
     res.render('accounts/login', {title: 'Login', errors: null})
 }
 
 export const get_sign_up_form = (req, res) =>{
-    res.render('accounts/sign-up', { title: 'Sign Up'})
+    res.render('accounts/sign-up', { title: 'Sign Up', errors: null})
 }
 
 export const isLoggedIn = (req, res, next) => {
