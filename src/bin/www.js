cd0cd9d886f952afe7448f9a150c6263ddeab83a
GHOST-Aram <owinoojala@gmail.com>
2023-11-17 16:22:33 +0300

    chore: Log localhost url

--- src/bin/www.js
+++ src/bin/www.js
@@ -22,7 +22,9 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+server.listen(port, () =>{
+  console.log('Running on http://localhost:8000')
+});
 server.on('error', onError);
 server.on('listening', onListening);
 
